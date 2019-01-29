97d960e2043c78b34d18e6d40c458bfc9313b7cb
christopherlobianco <christopher.lobianco15@gmail.com>
2019-01-28 19:02:08 -0500

    Commit 01/28/19 (19:02)

---
+++ src/BioKit.java
@@ -0,0 +1,22 @@
+/*
+ * @filename: BioKit
+ * @author: Christopher LoBianco
+ * @version: 1.0 (01/28/18)
+ */
+
+import java.io.File;
+
+public class BioKit {
+
+    //Fields
+    public File currentFile;
+
+    //Constructor
+    public BioKit() {
+
+    }
+
+    //Methods
+
+
+}
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,29 +1,9 @@
 /*
- * @filename: UserInterface.java
+ * @filename: UserInterface
  * @author: Christopher LoBianco
- * @version: 1.0 (01/01/18)
+ * @version: 1.0 (01/28/18)
  */
-import java.io.File;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileSystemView;
 
 public class UserInterface {
 
-    public static void main(String[] args) {
-
-        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-
-        int returnValue = jfc.showOpenDialog(null);
-        // int returnValue = jfc.showSaveDialog(null);
-
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = jfc.getSelectedFile();
-            System.out.println(selectedFile.getAbsolutePath());
-
-        }
-
-    }
 }
-
-
-
